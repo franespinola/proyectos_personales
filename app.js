02907d88a96884d2c4ec92174e3174e2e034e7fa
francisco espinola <franespinola_92@hotmail.com>
2021-06-21 19:01:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const publicFolderPath = path.resolve(__dirname +'/public/css/styles.css');
 app.use('/public/css/styles.css',express.static(publicFolderPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/home.html'));
+    res.sendFile(path.join(__dirname, './views/home.html'));
 })
 app.get('/register.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/register.html'));
+    res.sendFile(path.join(__dirname, './views/register.html'));
 })
 app.get('/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/login.html'));
+    res.sendFile(path.join(__dirname, './views/login.html'));
 })
 
 app.use('/public/img', express.static(__dirname +'/public/img'));
